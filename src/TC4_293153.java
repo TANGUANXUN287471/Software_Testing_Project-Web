97ec375b73e55eb9d91a8425761acabb4f915670
Choong Qian Yu <choong17qianyu4@gmail.com>
2024-06-28 00:25:30 +0800

    Update

--- src/TC4_293153.java
+++ src/TC4_293153.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.time.Duration;
 
@@ -31,35 +32,54 @@ public class TC4_293153 { //Review Role
 
     // CMS_16: Test Case for Register Reviewer Account
     @Test
-    public void testSearchPaper() throws InterruptedException {
-        // Open a website
+    public void testRegisterAccount() {
+        // Open the login page
         driver.get("https://sandbox.soc-conferences.com/reviewer/login.php");
-        Thread.sleep(1000);
 
+        // Wait for the "Create here" link to be present in the DOM
+        WebElement createHereLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/p/a[1]")));
 
-        //Click on "details"
-        WebElement createHereLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[2]/p/a[1]")));
-        createHereLink.click();
-        Thread.sleep(1000);
+        // Use JavaScript to click the link
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", createHereLink);
+
+        // Wait for the new page to load
+        wait.until(ExpectedConditions.urlToBe("https://sandbox.soc-conferences.com/reviewer/register.php"));
 
 
         // Fill the registration form
         //Expertise
+        WebElement selectExp = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/div[1]/div[2]/div"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectExp)).click();
 
-        new Select(driver.findElement(By.xpath("//*[@id='idfield']"))).selectByVisibleText("Field Name");
-        new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/div[2]/div[1]/select"))).selectByVisibleText("Title");
+        WebElement f1 = driver.findElement(By.xpath("//*[@id=\"selectid1\"]"));
+        wait.until(ExpectedConditions.elementToBeClickable(f1)).click();
+
+        WebElement selected = driver.findElement(By.xpath("//*[@id=\"idsubmitfields\"]/div/div/button"));
+        wait.until(ExpectedConditions.elementToBeClickable(selected)).click();
+
         driver.findElement(By.xpath("//*[@id='idname']")).sendKeys("John Doe");
         driver.findElement(By.xpath("//*[@id='idemail']")).sendKeys("john.doe@example.com");
         driver.findElement(By.xpath("//*[@id='idphone']")).sendKeys("123456789");
         driver.findElement(By.xpath("//*[@id='idorg']")).sendKeys("Organization Name");
         driver.findElement(By.xpath("//*[@id='idaddress']")).sendKeys("Mailing Address");
         driver.findElement(By.xpath("//*[@id='idurl']")).sendKeys("http://example.com");
-        new Select(driver.findElement(By.xpath("//*[@id='country']"))).selectByVisibleText("Country Name");
-        driver.findElement(By.xpath("//*[@id='idtitle']")).sendKeys("/path/to/file");
+        new Select(driver.findElement(By.xpath("//*[@id='country']"))).selectByVisibleText("Malaysia");
+        new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/div[2]/div[1]/select"))).selectByVisibleText("Professor");
+
         driver.findElement(By.xpath("//*[@id='idpass']")).sendKeys("password123");
         driver.findElement(By.xpath("//*[@id='idpassb']")).sendKeys("password123");
-        driver.findElement(By.xpath("//*[@id='recaptcha-anchor']/div[1]")).click(); // Assuming reCAPTCHA can be bypassed for test
-        driver.findElement(By.xpath("//*[@id='idsubmit']")).click();
+        // Wait for the reCAPTCHA to be present in the DOM
+        WebElement recaptchaCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='recaptcha-anchor']/div[1]")));
+
+        // Use JavaScript to click the reCAPTCHA checkbox
+        JavascriptExecutor jsrc = (JavascriptExecutor) driver;
+        jsrc.executeScript("arguments[0].click();", recaptchaCheckbox);
+
+        // Wait for a short time to ensure the reCAPTCHA is processed (usually you need to solve it manually)
+
+        driver.findElement(By.xpath("//*[@id=\"recaptcha-anchor\"]/div[1]")).click(); // Assuming reCAPTCHA can be bypassed for test
+        driver.findElement(By.xpath("//*[@id=\"idsubmit\"]")).click();
 
         // Handle confirmation alert
         Alert confirmAlert = wait.until(ExpectedConditions.alertIsPresent());
@@ -92,7 +112,7 @@ public class TC4_293153 { //Review Role
         // Click "Login" button
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/p[4]/button")).click();
         alert.accept();
-        // Wait for the login process to complete	
+        // Wait for the login process to complete
         Thread.sleep(2000);
 
 
@@ -105,20 +125,19 @@ public class TC4_293153 { //Review Role
 
 
         // Click the “News” button
-        WebElement newsButton = driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/div/table/tbody/tr[4]/td[6]/a/button"));
+        WebElement newsButton = driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/div/table/tbody/tr[6]/td[6]/a/button"));
         wait.until(ExpectedConditions.elementToBeClickable(newsButton)).click();
 
         // Sleep for 1 seconds
         Thread.sleep(1000);
 
         // Click the “Download” button
-        WebElement downloadButton = driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/div/div[1]/div/div/table/tbody/tr[4]/td/a/button"));
+        WebElement downloadButton = driver.findElement(By.xpath("//*[@id=\"newsdetails5\"]/div/div/table/tbody/tr[4]/td/a/button"));
         wait.until(ExpectedConditions.elementToBeClickable(downloadButton)).click();
 
         // Sleep for 1 seconds
         Thread.sleep(1000);
 
-        driver.quit();
     }
 
     // Check download success (assumed to be verified by the presence of a specific element or text)
@@ -144,7 +163,7 @@ public class TC4_293153 { //Review Role
         // Click "Login" button
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/p[4]/button")).click();
         alert.accept();
-        // Wait for the login process to complete	
+        // Wait for the login process to complete
         Thread.sleep(2000);
 
         // Click the "review” tab
@@ -193,7 +212,7 @@ public class TC4_293153 { //Review Role
         // Click "Login" button
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/p[4]/button")).click();
         alert.accept();
-        // Wait for the login process to complete	
+        // Wait for the login process to complete
         Thread.sleep(2000);
 
         // Click the “Review” tab
@@ -251,7 +270,7 @@ public class TC4_293153 { //Review Role
         wait.until(ExpectedConditions.elementToBeClickable(conferenceOrganizer)).sendKeys("Good");
 
         // Click the “Submit” button
-        WebElement submitButton = driver.findElement(By.xpath("/html/body/a/div[1]/form/div/div[5]/div[3]/button"));
+        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"idsubmit\"]"));
         wait.until(ExpectedConditions.elementToBeClickable(submitButton)).click();
 
         // Handle confirmation alert
@@ -291,7 +310,7 @@ public class TC4_293153 { //Review Role
         // Click "Login" button
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/p[4]/button")).click();
         alert.accept();
-        // Wait for the login process to complete	
+        // Wait for the login process to complete
         Thread.sleep(2000);
 
         // Click the “Messages” tab
